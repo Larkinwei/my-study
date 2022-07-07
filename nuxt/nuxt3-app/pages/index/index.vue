e66571dd5ffb0f34a38bb949935a9f2fc103b81c
larkinwei <liujianwei@babybus.com>
2022-07-07 15:10:44 +0800

    feat: 学习nuxt

---
+++ nuxt/nuxt3-app/pages/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Index Page  afsdjflsdj  
+    <msg />
+    <NuxtLink to="/about">跳转至 About 页面</NuxtLink>
+  </div>
+</template>
+<script>
+  export default {
+    created() {
+      console.log('created')
+      console.log(util)
+    },
+  }
+</script>
